ee867461ecff12dd4f0477f3ba6b51f49be53051
Xiaoping-Li <narci2008@gmail.com>
2019-07-03 12:03:10 -0700

    Init react-ts-app

--- greeter.ts
+++ greeter.ts
@@ -1,9 +1,9 @@
-class Student {
-  fullName: string;
-  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
-  }
-}
+// class Student {
+//   fullName: string;
+//   constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//     this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+//   }
+// }
 
 interface Person {
   firstName: string,
